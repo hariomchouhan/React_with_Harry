013ede0afbbbf725fa88cbf275b718caa6dea35f
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-05-28 22:28:45 +0530

    Changing title dynmically & adding favicons

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -11,31 +11,50 @@ function App() {
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
   const toggleMode = () => {
     if (Mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = "#042743"
-      showAlert("Dark mode has been enabled", "success")
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      
+      // This code is change or toggle the mode in the title
+      // setTimeout(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setTimeout(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
     } else {
       setMode("light");
-      document.body.style.backgroundColor = "white"
-      showAlert("Light mode has been enabled", "success")
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
   };
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-      <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} showAlert={showAlert} />
+      <Navbar
+        title="TextUtils"
+        mode={Mode}
+        toggleMode={toggleMode}
+        showAlert={showAlert}
+      />
       {/* <Navbar /> */}
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={Mode}/>
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze below"
+          mode={Mode}
+        />
       </div>
     </>
   );
--- textutils/src/components/Alert.js
+++ textutils/src/components/Alert.js
@@ -7,7 +7,7 @@ function Alert(props) {
     }
   return (
     props.alert && <div
-      className={`container alert alert-${props.alert.type} alert-dismissible fade show`}
+      className={`container my-2 alert alert-${props.alert.type} alert-dismissible fade show`}
       role="alert"
     >
       <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
