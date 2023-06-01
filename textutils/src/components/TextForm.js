2f3dace1760f4047841ccfb756bf657b5773bf05
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-06-01 18:35:16 +0530

    Short logic of copy text of clipboard

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -39,10 +39,10 @@ export default function TextForm(props) {
 
   // To Copy the clipboard text
   const handleCopy = () => {
-    let text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    document.getSelection().removeAllRanges();
+    // let text = document.getElementById("myBox");
+    // text.select();
+    navigator.clipboard.writeText(text);
+    // document.getSelection().removeAllRanges();
     props.showAlert("Copy clipboard", "success")
   };
 
